0a0ba8b183606b9db6f40bd66109604174705d0c
Shailendra Verma <shailendravermag@gmail.com>
2021-01-03 22:10:28 +0000

    initial checkin

---
+++ src/main/java/com/sv/email/Email.java
@@ -0,0 +1,70 @@
+package com.sv.email;
+
+import com.sv.core.Utils;
+import com.sv.core.logger.MyLogger;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Email {
+
+    private static MyLogger logger;
+    private static Properties emailProp = new Properties();
+
+    static {
+        init();
+    }
+
+    private static void init() {
+        logger = MyLogger.createLogger("email.log");
+        emailProp = new Properties();
+        try {
+            emailProp.load(Email.class.getClassLoader().getResourceAsStream("email.properties"));
+        } catch (IOException e) {
+            logger.error("Unable to load email properties.", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String user = System.getenv("TEST_GMAIL_USER");
+        EmailDetails details = new EmailDetails(user, user, "subject");
+        details.setUser(user);
+        details.setPwd(System.getenv("TEST_GMAIL_PWD"));
+        details.setBody("Test");
+        send(details);
+    }
+
+    private static Session authenticate(String user, String pwd) {
+        return Session.getInstance(emailProp,
+                new javax.mail.Authenticator() {
+                    protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+                        return new javax.mail.PasswordAuthentication(user, pwd);
+                    }
+                });
+    }
+
+    public static void send(EmailDetails details) {
+
+        try {
+            Message message = new MimeMessage(authenticate(details.getUser(), details.getPwd()));
+            message.setFrom(new InternetAddress(details.getFrom()));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(details.getTo())
+            );
+            message.setSubject(details.getSubject());
+            message.setText(details.getBody());
+
+            Transport.send(message);
+            logger.log("Email send " + details);
+        } catch (MessagingException e) {
+            logger.error("Email failed!! " + details, e);
+        }
+    }
+}
---
+++ src/main/java/com/sv/email/EmailDetails.java
@@ -0,0 +1,70 @@
+package com.sv.email;
+
+public class EmailDetails {
+
+    private String to, from, subject, body, user, pwd;
+
+    public EmailDetails(String to, String from, String subject) {
+        this.to = to;
+        this.from = from;
+        this.subject = subject;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    @Override
+    public String toString() {
+        return "EmailDetails{" +
+                "to='" + to + '\'' +
+                ", from='" + from + '\'' +
+                ", subject='" + subject + '\'' +
+                ", user='" + user + '\'' +
+                '}';
+    }
+}
